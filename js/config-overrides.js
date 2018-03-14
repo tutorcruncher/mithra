31a5575c0b9bd071a595bf6818080d090d75e1e8
Samuel Colvin <s@muelcolvin.com>
2018-03-14 19:00:34 +0000

    cleaning up docker build and js

--- js/config-overrides.js
+++ js/config-overrides.js
@@ -1,5 +1,30 @@
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
+const path = require('path')
+
+module.exports = function override (config, env) {
+
+  // remove the css loader to simplify things
+  config.module.rules[1].oneOf = config.module.rules[1].oneOf.filter(function (l) {
+    const test = l.test && l.test.toString()
+    return test !== /\.css$/
+  })
+  // compile sass, this comes first and compresses css as well as loading sass/scss
+  // https://github.com/facebookincubator/create-react-app/issues/2498
+  config.module.rules[1].oneOf.splice(0, 0,
+    {
+        test: /\.(sass|scss|css)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              outputStyle: 'compressed',
+              includePaths: [path.resolve(__dirname, 'node_modules')],
+            }
+          }
+        ]
+    },
+  )
   // console.dir(config, { depth: 10, colors: true })
-  return config;
+  return config
 }
---
+++ run-docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "running mithra locally for development and testing"
+echo "You'll want to run docker-logs in anther window see what's going on"
+echo "==================================================================="
+echo ""
+echo "building js..."
+
+cd js
+yarn build
+cd ..
+
+echo "==================================================================="
+echo ""
+echo "running docker compose..."
+
+docker-compose up -d --build
